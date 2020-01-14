ae220e018289fad3df48dc6b18eb1ae4382ca172
Georgia <gabadonsky@gmail.com>
2020-01-14 17:40:00 -0500

    final version

--- fastascript.py
+++ fastascript.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import argparse
-import FileIO
-import Random
-import OS
+import os
 import random
 #importing libraries
 
-#parser = argparse.ArgumentParser
+#parser = argparse.ArgumentParser(description = "please give a header")
+#parser.add_argument("-h", "-header")
+#args = parser.parse_args()
+
 nuc_list= ["A","T","C","G"]
 
 def repeat_seq(length):
@@ -23,10 +24,25 @@ repeat_seq5=repeat_seq(30)
 def new_fasta(): 
 #try block is skipping error and makes the code continue on with what it can
 	try:
-		with open("psuedo_protein.fasta",w) as in_handle:
-			in_handle.write(repeat_seq1, repeat_seq2, repeat_seq3, repeat_seq4, repeat_seq5)
+		with open("psuedo_protein.fasta","w") as in_handle:
+			#in_handle.write(args.header)
+#			print(in_handle)
+#			in_handle.write("\r")
+#			print(in_handle)
+			in_handle.write(repeat_seq1)
+			in_handle.write(repeat_seq1)
+			in_handle.write(repeat_seq2)
+			in_handle.write(repeat_seq3)
+			in_handle.write(repeat_seq4)
+			in_handle.write(repeat_seq5)
+			in_handle.write(repeat_seq2)
+			in_handle.write(repeat_seq3)
+			in_handle.write(repeat_seq4)
+			in_handle.write(repeat_seq5)
+		return(in_handle)
 
-
-	except as IOError:
-	#this is the exception error that will help the code
+	except IOError as err:
+#this is the exception error that will help the code
 		print("IOError")
+#calling function
+print(new_fasta())
